343f67dc6a47188e603a25d3be4c8721ee0bd98f
Anderson Rolf <email.the.anderson@gmail.com>
2019-04-24 17:00:57 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,7 +10,7 @@ def input_to_index
   puts "Welcome to Tic Tac Toe!"
   puts "Where would you like to go?"
   user_input = gets.strip
-  # user_input .to_i
+  user_input .to_i
   # user_input -= 1
   # space = user_input - 1
   # space .to_i
