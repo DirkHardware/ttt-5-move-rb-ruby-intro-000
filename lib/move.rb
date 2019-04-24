972f40ecb30bf1621a36a29888e89f524a30ce5d
Anderson Rolf <email.the.anderson@gmail.com>
2019-04-24 17:01:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index
   puts "Where would you like to go?"
   user_input = gets.strip
   user_input .to_i
-  user_input -= 1
+  user_input = user_input - 1
   # space = user_input - 1
   # space .to_i
   # board[space] = X
